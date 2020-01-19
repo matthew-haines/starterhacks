ae2c64bfd337bc554e0f953746395ddb08f09242
RaymondR-Wu <raymond.w.78.in@gmail.com>
2020-01-18 20:41:56 -0500

    added csv

--- collection/getdata.py
+++ collection/getdata.py
@@ -2,13 +2,24 @@ import sys
 from serial import Serial
 import time
 import csv
+import pandas
 
 if __name__ == '__main__':
     # parameters are device, baud, and then output file
-    if len(sys.argv) != 3:
+    if len(sys.argv) != 4:
         print("Bad parameters")
 
     ser = Serial(sys.argv[1], sys.argv[2])
     data = []
     time.sleep(2)
-    print(ser.readline())
+    for i in range(0,1000):
+
+        temp = ser.readline().decode().rstrip()
+        x, y, z = temp.split(' ')
+        data.append({'x': x, 'y': y, 'z': z})
+        time.sleep(1/60)
+        
+    df = pandas.DataFrame(data)
+
+    df.to_csv(sys.argv[3], sep=',',index=False)
+
